33730fd3cfc129d3b198462745ad666ed1e95f7f
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2017-07-20 02:54:48 -0400

    set up package.json using npm init npm install lodash; node modules gitignored

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ console.log('Revising node js tutorial');
 const fs = require('fs');
 const os = require('os');
 const testReq = require('./notes.js');
+const _ = require('lodash');
 
-
-console.log(testReq.myMessage);
-console.log(testReq.myFunc(2,3));
+console.log(_.uniq(['st1','st1','st1',2,2,3,3,4,5,6,'st2']));//removes duplicates
+// console.log(testReq.myMessage);
+// console.log(testReq.myFunc(2,3));
 //var user = os.hostname();
 //fs.appendFile('greetings.txt',`${user},Congrats you just did the first IO ops in NodeJS!`);
 //console.log(user);
