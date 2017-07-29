36f4a6e70109fab7ed4c667d00e0396c3a67408a
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2017-07-29 16:46:24 -0400

    Using Yargs and key value pairs Arrow function exports array,

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ console.log('Revising node js tutorial');
 
 //const fs = require('fs');
 //const os = require('os');
-const testReq = require('./notes.js');//Note will execute notes.js console log
-//const yargs = require('yargs');
+const yargs = require('yargs');
+//First node system requires, then npm modules and then local. Use a line break;
+const notes = require('./notes.js');//Note will execute notes.js console log
 
 //Using command line arguments
 var command = process.argv;//NOte arguments are stored in process object
 //First argument is node and second is app.js in this case
 console.log('command line argument'+command[2]);
-
 //WHere command line struggles
 console.log('Will need to parse in order to fetch key value pairs',command[3]);
 //const _ = require('lodash');
 
-//console.log(_.uniq(['st1','st1','st1',2,2,3,3,4,5,6,'st2']));//removes duplicates
-// console.log(testReq.myMessage);
-// console.log(testReq.myFunc(2,3));
-//var user = os.hostname();
-//fs.appendFile('greetings.txt',`${user},Congrats you just did the first IO ops in NodeJS!`);
-//console.log(user);
+//Using yargs
+var yarg = yargs.argv;
+console.log('yargs commands ', yarg);
+console.log('yargs first command ',yarg._[0]);
+console.log('key value pair: ',yarg.title);
+
+if(yarg._[0]=='add'){
+  notes.add(yarg.title,yarg.body);
+}else if(yarg._[0]=='list'){
+  notes.list();
+}
--- notes.js
+++ notes.js
@@ -1,15 +1,21 @@
 console.log('Own file which is required in app.js');
 
-//console.log(module);
 
-module.exports.myMessage = 'Hey this is Great!';
-
-// module.exports.myFunc = function(){
-//   console.log('inside my func');
-//   return ' returning inside my func';
-// }
-
-module.exports.myFunc = (a,b) => {
-  console.log('sum of numbers = '+(a+b));
-  return a+b;
+var AddNote = (title, body) => {
+  console.log('Adding note with title ',title,' and body ',body);
 }
+
+var getAllNotes = () => {
+  console.log('Will add code here to get all notes');
+}
+//when the name value pair naming is the same
+// module.exports ={
+//   AddNote,
+//   getAllNotes
+// };
+
+//or if the mapping to the name is different ES6
+module.exports ={
+  add: AddNote,
+  list: getAllNotes
+};
