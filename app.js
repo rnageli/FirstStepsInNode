b3f17b6c7658865fb22e338c68e59748a6550356
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2017-07-29 15:18:01 -0400

    Using command line arguments and understanding why we need yargs

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 console.log('Revising node js tutorial');
 
-const fs = require('fs');
-const os = require('os');
-const testReq = require('./notes.js');
-const _ = require('lodash');
+//const fs = require('fs');
+//const os = require('os');
+const testReq = require('./notes.js');//Note will execute notes.js console log
+//const yargs = require('yargs');
 
-console.log(_.uniq(['st1','st1','st1',2,2,3,3,4,5,6,'st2']));//removes duplicates
+//Using command line arguments
+var command = process.argv;//NOte arguments are stored in process object
+//First argument is node and second is app.js in this case
+console.log('command line argument'+command[2]);
+
+//WHere command line struggles
+console.log('Will need to parse in order to fetch key value pairs',command[3]);
+//const _ = require('lodash');
+
+//console.log(_.uniq(['st1','st1','st1',2,2,3,3,4,5,6,'st2']));//removes duplicates
 // console.log(testReq.myMessage);
 // console.log(testReq.myFunc(2,3));
 //var user = os.hostname();
