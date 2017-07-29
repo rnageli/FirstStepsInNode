a424a78f1125d4aa2a6132499ffa1e51badff4ce
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2017-07-29 18:06:11 -0400

    Reading and writing to a file JSON data. Parse and stringify

--- playground/json.js
+++ playground/json.js
@@ -1,11 +1,20 @@
-// to convert json string to an object
+// writing and readung json data
 
-var myName = '{"name":"Ravi","age":23}';
+var fs = require('fs');
 
-var person = JSON.parse(myName);
-//Prints just the opposite of the previous commit
-//parse and JSON.stringify
-console.log(typeof myName);//string
-console.log(myName);//{"name":"Ravi","age":23}
-console.log(typeof person);//object
-console.log(person);//{name:'Ravi',age:23}
+var originalNote = {
+  title : 'First Note',// Keep in mind that : is  to be used and not =
+  body : 'Things are getting a little bit heated.'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync("notes.json",originalNoteString);
+
+console.log('writing to file success. Starting to read from file notes.json');
+
+var noteString = fs.readFileSync('notes.json');
+console.log('Read from fiel finished.');
+var noteObj = JSON.parse(noteString);
+
+console.log('Reading from the note obj: Title is ',noteObj.title);
